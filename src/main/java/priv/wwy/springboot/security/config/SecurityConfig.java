d47c34bede2a5f0188e8024448500f3b536d997a
wangweiye <wangweiye@wo.cn>
2021-06-25 15:21:11 +0800

    2nd committed with a clear farmework.

--- src/main/java/priv/wwy/springboot/security/config/SecurityConfig.java
+++ src/main/java/priv/wwy/springboot/security/config/SecurityConfig.java
@@ -1,37 +1,50 @@
 package priv.wwy.springboot.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author wangweiye
  * @date 2021-06-24 15:27
  */
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Bean
-    @Override
-    public UserDetailsService userDetailsServiceBean() throws Exception{
-        return  super.userDetailsServiceBean();
-    }
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception{
-        return super.authenticationManagerBean();
-    }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("wangdaye").password("123456").roles("USER")
-                .and()
-                .withUser("admin").password("admin").roles("USER","ADMIN")
-                .and()
-                .passwordEncoder(new CustomPasswordEncoder());
 
+    @Bean
+    public UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("wangdaye").password("123456").authorities("USER").build());
+        manager.createUser(User.withUsername("admin").password("123").authorities("ADMIN").build());
+        return  manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .antMatchers("/hello/**").hasRole("USER")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().defaultSuccessUrl("/login-success").permitAll();
+                //.and()
+                //.httpBasic();
     }
 }
 
--- src/main/java/priv/wwy/springboot/security/controller/ServiceProvider.java
+++ src/main/java/priv/wwy/springboot/security/controller/ServiceProvider.java
@@ -2,6 +2,7 @@ package priv.wwy.springboot.security.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,12 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021-06-24 15:25
  */
 @RestController
+//@RequestMapping("/auth")
 public class ServiceProvider {
     @Value("${server.port}")
     String port;
     @GetMapping("/hello")
     public String getHelloMessage(){
         return "Hello WANGDAYE! from "+port+" !";
+    }
+    @GetMapping("/login-success")
+    public String loginSuccess(){
+        return "To login successfully!";
     }
 }
 
